b7efa33755aec25db33778a79364ad5d5e8e86a7
Felipe <felipe_orteganav@hotmail.com>
2026-03-02 13:38:03 -0600

    first commit

---
+++ Estructuras de Datos 2026/20Marzo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Felipe Ortega on 02/03/2026.
+//
+
+#include "20Marzo.h"
---
+++ Estructuras de Datos 2026/20Marzo.h
@@ -0,0 +1,8 @@
+//
+// Created by Felipe Ortega on 02/03/2026.
+//
+
+#ifndef INC_20MARZO_H
+#define INC_20MARZO_H
+
+#endif //INC_20MARZO_H
